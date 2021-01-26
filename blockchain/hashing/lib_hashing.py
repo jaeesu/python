b7e87b50882e51f3712491b9ac06b8a9fc242956
jaeesu <wogmlsu@gmail.com>
2021-01-26 22:44:33 +0900

    startup_blockchain_hashing

---
+++ blockchain/hashing/lib_hashing.py
@@ -0,0 +1,12 @@
+#python library : hash()
+#battery include -> 프로그램 재시작마다 해시 값이 달라짐 -> 일시적 메모리 저장에 사용
+#재시작마다 달라져 데이터를 찾을 수 없다
+
+print(hash('Kim'))
+print(hash('John'))
+print(hash('Smith'))
+print(hash('Michael'))
+print(hash('Raymond'))
+print(hash('Clayton'))
+
+
---
+++ blockchain/hashing/test_extended_hashing.py
@@ -0,0 +1,13 @@
+from extended_hashing import Hashtable
+
+boo = Hashtable()
+boo.put('Kim', 7458)
+boo.put('John', 8569)
+boo.put('Smith', 1452)
+boo.put('Michael', 2563)
+boo.put('Raymond', 1598)
+boo.put('Clayton', 7532)
+
+boo.show()
+
+#extended_hashing
