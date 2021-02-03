d23ccca54427159ebb55beef7e40a8977030413d
jaeesu <50989437+jaeesu@users.noreply.github.com>
2021-02-03 23:25:08 +0900

    Add files via upload

---
+++ boj_contribution/ex.py
@@ -0,0 +1,25 @@
+'''l_lst=[]
+i=0
+for i in range(50):
+    j=0
+    lst=[]
+    for j in range(5):
+        lst.append(i*5+j)
+        j+=1
+    l_lst.append(lst)
+    i+=1
+
+print(l_lst)
+print(type(l_lst))
+print(l_lst[len(l_lst)-1][3])
+
+
+sed = [[0]*7]*10
+print(type(sed))'''
+
+lst1 = [1,2,3,4,5]
+lst2 = [10,11,12,13,14]
+print(lst1+lst2)
+
+for i in range :
+    lst1[i] += lst2[i]
