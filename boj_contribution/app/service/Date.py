901dda23d0ab709d1d92c15049a81ed9210ff154
jaeesu <wogmlsu@gmail.com>
2021-02-04 23:13:00 +0900

    rm

--- boj_contribution/app/service/Date.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-def convert_datetime(unixtime):
-    #date = datetime.datetime.fromtimestamp(unixtime).strftime('%Y-%m-%d %H:%M:%S')
-    date = datetime.datetime.fromtimestamp(unixtime).strftime('%Y-%m-%d')
-    return date # format : str
-
-
